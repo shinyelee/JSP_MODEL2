9f3134f590fad64ea393fce88ecd72b0f60ee3e4
shinyelee <shinye0213@naver.com>
2020-09-15 23:51:27 +0900

    BoardReplyFormAction.java 생성

---
+++ src/action/BoardReplyFormAction.java
@@ -0,0 +1,25 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import svc.BoardDetailService;
+import vo.ActionForward;
+import vo.BoardBean;
+
+public class BoardReplyFormAction implements Action {
+
+	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		
+		ActionForward forward = new ActionForward();
+		String nowPage = request.getParameter("page");
+		int board_num = Integer.parseInt(request.getParameter("board_num")); // 답변글 등록 처리를 한 후 원래 보던 페이지로 되돌아 가야 하기 때문에 파라미터로 전송되어오는 페이지 번호 값을 얻어옴.
+		BoardDetailService boardDetailService = new BoardDetailService();
+		BoardBean article = boardDetailService.getArticle(board_num); // 답변글을 달아줄 대상 글의 정보를 얻어옴.
+		request.setAttribute("article", article); // 글 정보와
+		request.setAttribute("page", nowPage); // 페이지 번호를 request 영역에 속성으로 공유.
+		forward.setPath("/board/qna_board_reply.jsp");
+		return forward;
+
+	}
+	
+}
