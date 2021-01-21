c3f60f432d56f0e83a602a31ebecd151ba779c0b
shinyelee <shinye0213@naver.com>
2021-01-22 00:08:09 +0900

    GoodsCartQtyUpService 생성

---
+++ src/svc/GoodsCartQtyUpService.java
@@ -0,0 +1,26 @@
+package svc;
+
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import vo.Cart;
+
+// 장바구니 항목 수량 증가 요청을 처리하는 비즈니스 로직을 구현하는 Service 클래스.
+public class GoodsCartQtyUpService {
+
+	public void upCartQty(String name, HttpServletRequest request) { 
+		HttpSession session = request.getSession(); // 요청을 한 클라이언트의 세션 객체를 얻어옴.
+		// 세션 영역에 공유되어 있는 장바구니 목록 객체를 얻어옴.
+		ArrayList<Cart> cartList = (ArrayList<Cart>)session.getAttribute("cartList");
+		
+		// 수량을 증가시킬 장바구니 항목 객체를 name 값으로 비교해 검색한 후 해당 객체의 수량 값을 증가시킴.
+		for (int i = 0; i < cartList.size(); i++) {
+			if(cartList.get(i).getName().equals(name)) {
+				cartList.get(i).setQty(cartList.get(i).getQty()+1);		
+			}
+			
+		}
+		
+	}
+	
+}
