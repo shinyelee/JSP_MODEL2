9e514b81335d832399fc9ddffc0503317178cd9a
shinyelee <shinye0213@naver.com>
2021-01-22 22:37:45 +0900

    GoodsDAO.java 설명 추가

--- src/dao/GoodsDAO.java
+++ src/dao/GoodsDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
+// 데이터베이스 작업을 JdbcUtil 클래스에 정의돼 있는 static 메소드를 사용해 편리하게 처리하기 위해
+// JdbcUtil 클래스의 static 메소드나 속성을 임포트함.
 import static db.JdbcUtil.*;
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,14 +12,20 @@ public class GoodsDAO {
 	Connection con;
 	private static GoodsDAO boardDAO;
 	
-	private GoodsDAO() {
-		
-	}
+	// 생성자의 접근 제한자를 외부 클래스에서 직접 호출할 수 없도록 private으로 지정.
+	// (외부 클래스에서 GoodsDAO 객체의 메소드를 사용할 때마다 객체를 계속 생성하지 않고
+	// 싱글톤 패턴을 사용해 처음 생성된 객체를 공유해서 사용하도록 처리하기 위해서임.)
+	private GoodsDAO() {}
 	
+	// 데이터베이스 작업을 하기 위해서 필요한 Connection 객체를 멤버 변수 값으로 설정.
 	public void setConnection(Connection con){
 		this.con = con;
 	}
 	
+	// 외부 클래스에서 GoodsDAO 클래스에 정의된 메소드를 사용하기 위해 객체를 얻어갈 때
+	// 첫 번째 요청에서만 객체를 생성해주고 다음에 객체를 사용할 때는 처음 생성된 객체의 레퍼런스 값을 공유.
+	// 생성자의 접근 제한자를 private으로 지정했으므로 외부 클래스에서 GoodsDAO 객체의 레퍼런스 값을 얻어갈 땐
+	// 반드시 getInstance() 메소드를 호출해서 얻어가야 함.
 	public static GoodsDAO getInstance(){
 		
 		if(boardDAO == null){
@@ -27,6 +35,7 @@ public class GoodsDAO {
 		return boardDAO;
 	}
 	
+	// 데이터베이스에 저장돼 있는 모든 상품 정보를 반환하는 메소드를 정의.
 	public ArrayList<Goods> selectGoodsList() {
 		
 		PreparedStatement pstmt = null;
@@ -65,6 +74,7 @@ public class GoodsDAO {
 		return goodsList;
 	}
 	
+	// 데이터베이스에 저장돼 있는 상품 정보 중 파라미터로 전송된 id 값을 가지고 있는 상품의 정보를 반환하는 메소드를 정의.
 	public Goods selectGoods(int id) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -98,6 +108,7 @@ public class GoodsDAO {
 		return goods;
 	}
 	
+	// 상세 내용 보기 요청이 된 상품 정보의 조회수를 증가시키는 메소드를 정의.
 	public int updateReadCount(int id) {
 		PreparedStatement pstmt = null;
 		int updateCount = 0;
@@ -117,6 +128,7 @@ public class GoodsDAO {
 		return updateCount;
 	}
 	
+	// 새로운 상품 정보를 데이터베이스에 추가하는 메소드를 정의.
 	public int insertGoods(Goods goods) {
 		PreparedStatement pstmt = null;
 		int insertCount = 0;
