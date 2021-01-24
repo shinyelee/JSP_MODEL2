ebdac36b10312b5ecf40d36c731640a4b84ca96d
shinyelee <shinye0213@naver.com>
2021-01-24 19:59:54 +0900

    JdbcUtil.java 주석 추가

--- src/db/JdbcUtil.java
+++ src/db/JdbcUtil.java
@@ -5,19 +5,24 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
-// DB 관련 공통 기능 클래스.
+// DB 관련 기본 기능 클래스.
+// Connection(연결), Close(자원반환), Commit(작업완료), Rollback(취소) 등을 담당.
 public class JdbcUtil {
 
 	// Connection Pool에서 Connection 객체를 얻어와서 반환하는 메소드를 정의.
 	public static Connection getConnection() {
 		Connection con = null;
 		
-		try {
-			Context initCtx = new InitialContext(); // 톰캣 자체의 컨텍스트를 얻어옴.
-			Context envCtx = (Context)initCtx.lookup("java:comp/env"); // Resource 정의에 관한 컨텍스트를 얻어옴. lookup 메소드의 반환 타입이 Object이므로 Context 타입으로 다운캐스팅해야 함.
-			DataSource ds = (DataSource)envCtx.lookup("jdbc/MySQLDB"); // context.xml에서 정의한 DataSource 객체를 얻어옴.
-			con = ds.getConnection(); // Connection Pool에서 Connection 객체를 얻어옴.
-			con.setAutoCommit(false); // Connection 객체에 트랜잭션을 적용.
+		try { // 톰캣 자체의 컨텍스트를 얻어옴.
+			Context initCtx = new InitialContext();
+			// Resource 정의에 관한 컨텍스트를 얻어옴. lookup 메소드의 반환 타입이 Object이므로 Context 타입으로 다운캐스팅해야 함.
+			Context envCtx = (Context)initCtx.lookup("java:comp/env");
+			// context.xml에서 정의한 DataSource 객체를 얻어옴.
+			DataSource ds = (DataSource)envCtx.lookup("jdbc/MySQLDB");
+			// Connection Pool에서 Connection 객체를 얻어옴.
+			con = ds.getConnection();
+			// Connection 객체에 트랜잭션을 적용.
+			con.setAutoCommit(false);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
