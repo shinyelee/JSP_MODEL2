2cabca6126fc675f7c7c6e3c22e813df8c604f05
shinyelee <shinye0213@naver.com>
2021-01-27 23:25:38 +0900

    GoodsViewService.java -> MdViewService.java

--- src/action/MdViewAction.java
+++ src/action/MdViewAction.java
@@ -16,13 +16,13 @@ public class MdViewAction implements Action {
 		// 상품 정보 상세 보기 비즈니스 로직을 처리하는 서비스 객체를 생성.
 		MdViewService mdViewService = new MdViewService();
 		// 상세 정보를 출력할 대상 상품의 id 값을 파라미터로 받음.
-		int id = Integer.parseInt(request.getParameter("id"));
+		int mdid = Integer.parseInt(request.getParameter("mdid"));
 		// 파라미터 값으로 전송된 id 값을 가지고 있는 상품의 정보를 Md 클래스 객체 타입으로 반환받음.
-		Md md = mdViewService.getMdView(id);
+		Md md = mdViewService.getMdView(mdid);
 		// request 영역에 md 객체를 속성으로 공유.
 		request.setAttribute("md", md);
 		// 상품 정보의 이미지 이름 문자열을 today 문자열 뒤에 해당 상품의 id 값을 연결하여 ("today"+id) 쿠키 이름을 지정한 후 쿠키 객체를 생성하여 저장.
-		Cookie todayImageCookie = new Cookie("today"+id, md.getImage());
+		Cookie todayImageCookie = new Cookie("today"+mdid, md.getImage());
 		// 오늘 본 상품 이미지를 저장한 쿠키 객체가 클라이언트 시스템에 저장되어 있을 기간을 24시간으로 설정.
 		todayImageCookie.setMaxAge(60*60*24);
 		// 응답에 쿠키 객체를 추가.
