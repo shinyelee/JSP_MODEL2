8f63bc996d1bdb608ee1870d90d6d15b1e3635c8
shinyelee <shinye0213@naver.com>
2021-01-27 22:42:58 +0900

    GoodsDAO.java -> MdDAO.java

--- src/dao/MdDAO.java
+++ src/dao/MdDAO.java
@@ -5,17 +5,19 @@ package dao;
 import static db.JdbcUtil.*;
 import java.sql.*;
 import java.util.ArrayList;
-import vo.Goods;
+import vo.Md;
 
-public class GoodsDAO {
-
+public class MdDAO {
+	
 	Connection con;
-	private static GoodsDAO boardDAO;
+	private static MdDAO boardDAO;
 	
 	// 생성자의 접근 제한자를 외부 클래스에서 직접 호출할 수 없도록 private으로 지정.
 	// (외부 클래스에서 GoodsDAO 객체의 메소드를 사용할 때마다 객체를 계속 생성하지 않고
-	// 싱글톤 패턴을 사용해 처음 생성된 객체를 공유해서 사용하도록 처리하기 위해서임.)
-	private GoodsDAO() {}
+	// 싱글톤 패턴을 사용해 처음 생성된 객체를 공유해서 사용하도록 처리하기 위해서임.)	
+	private MdDAO() {
+		
+	}
 	
 	// 데이터베이스 작업을 하기 위해서 필요한 Connection 객체를 멤버 변수 값으로 설정.
 	public void setConnection(Connection con){
@@ -26,40 +28,39 @@ public class GoodsDAO {
 	// 첫 번째 요청에서만 객체를 생성해주고 다음에 객체를 사용할 때는 처음 생성된 객체의 레퍼런스 값을 공유.
 	// 생성자의 접근 제한자를 private으로 지정했으므로 외부 클래스에서 GoodsDAO 객체의 레퍼런스 값을 얻어갈 땐
 	// 반드시 getInstance() 메소드를 호출해서 얻어가야 함.
-	public static GoodsDAO getInstance(){
+	public static MdDAO getInstance(){
 		
 		if(boardDAO == null){
-		   boardDAO = new GoodsDAO();
+		   boardDAO = new MdDAO();
 		}
 		
 		return boardDAO;
 	}
 	
 	// 데이터베이스에 저장돼 있는 모든 상품 정보를 반환하는 메소드를 정의.
-	public ArrayList<Goods> selectGoodsList() {
-		
+	public ArrayList<Md> selectMdList() {
 		PreparedStatement pstmt = null;
-		ResultSet rs= null;
-		ArrayList<Goods> goodsList = null;
+		ResultSet rs = null;
+		ArrayList<Md> mdList = null;
 		
 		try {
-			pstmt = con.prepareStatement("SELECT * FROM goods");
+			pstmt = con.prepareStatement("SELECT * FROM md");
 			rs = pstmt.executeQuery();
 			
 			if(rs.next()){
-				goodsList = new ArrayList<Goods>();
+				mdList = new ArrayList<Md>();
 				
 				do {
-					goodsList.add(new Goods(
-							rs.getInt("id")
-							,rs.getString("name")
+					mdList.add(new Md(
+							 rs.getInt("mdid")
+							,rs.getString("item")
 							,rs.getInt("price")
 							,rs.getString("image")
-							,rs.getString("category")
-							,rs.getString("category2")
-							,rs.getInt("version")
+							,rs.getString("cate1")
+							,rs.getString("cate2")
+							,rs.getString("cate3")
 							,rs.getString("content")
-							,rs.getInt("readcount")));
+							,rs.getInt("hit")));
 				} while (rs.next());
 				
 			}
@@ -71,31 +72,31 @@ public class GoodsDAO {
 			close(pstmt);
 		}
 		
-		return goodsList;
+		return mdList;
 	}
 	
 	// 데이터베이스에 저장돼 있는 상품 정보 중 파라미터로 전송된 id 값을 가지고 있는 상품의 정보를 반환하는 메소드를 정의.
-	public Goods selectGoods(int id) {
+	public Md selectMd(int mdid) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		Goods goods = null;
+		Md md = null;
 		
 		try {
-			pstmt = con.prepareStatement("SELECT * FROM goods WHERE id=?");
-			pstmt.setInt(1, id);
+			pstmt = con.prepareStatement("SELECT * FROM md WHERE mdid=?");
+			pstmt.setInt(1, mdid);
 			rs = pstmt.executeQuery();
 			
 			if(rs.next()){
-				goods = new Goods(
-						rs.getInt("id")
-						,rs.getString("name")
+				md = new Md(
+						 rs.getInt("mdid")
+						,rs.getString("item")
 						,rs.getInt("price")
 						,rs.getString("image")
-						,rs.getString("category")
-						,rs.getString("category2")
-						,rs.getInt("version")
+						,rs.getString("cate1")
+						,rs.getString("cate2")
+						,rs.getString("cate3")
 						,rs.getString("content")
-						,rs.getInt("readcount"));
+						,rs.getInt("hit"));
 			}
 			
 		} catch (SQLException e) {
@@ -105,19 +106,19 @@ public class GoodsDAO {
 			close(rs);
 		}
 		
-		return goods;
+		return md;
 	}
 	
 	// 상세 내용 보기 요청이 된 상품 정보의 조회수를 증가시키는 메소드를 정의.
-	public int updateReadCount(int id) {
+	public int updateHit(int mdid) {
 		PreparedStatement pstmt = null;
 		int updateCount = 0;
 		String sql = "";
 		
 		try {
-			sql = "UPDATE goods SET readcount = readcount + 1 WHERE id=?";
+			sql = "UPDATE md SET hit = hit + 1 WHERE mdid=?";
 			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, id);
+			pstmt.setInt(1, mdid);
 			updateCount = pstmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -129,22 +130,22 @@ public class GoodsDAO {
 	}
 	
 	// 새로운 상품 정보를 데이터베이스에 추가하는 메소드를 정의.
-	public int insertGoods(Goods goods) {
+	public int insertMd(Md md) {
 		PreparedStatement pstmt = null;
 		int insertCount = 0;
 		String sql = "";
 		
 		try {
-			sql = "INSERT INTO goods VALUES(goods_seq.nextval,?,?,?,?,?,?,?,?)";
+			sql = "INSERT INTO md VALUES(md_seq.nextval,?,?,?,?,?,?,?,?)";
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, goods.getName());
-			pstmt.setInt(2, goods.getPrice());
-			pstmt.setString(3, goods.getImage());
-			pstmt.setString(4, goods.getCategory());
-			pstmt.setString(5, goods.getCategory2());
-			pstmt.setInt(6, goods.getVersion());
-			pstmt.setString(7, goods.getContent());
-			pstmt.setInt(8, goods.getReadcount());
+			pstmt.setString(1, md.getItem());
+			pstmt.setInt(2, md.getPrice());
+			pstmt.setString(3, md.getImage());
+			pstmt.setString(4, md.getCate1());
+			pstmt.setString(5, md.getCate2());
+			pstmt.setString(6, md.getCate3());
+			pstmt.setString(7, md.getContent());
+			pstmt.setInt(8, md.getHit());
 			insertCount = pstmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
