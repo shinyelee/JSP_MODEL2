f4f6271e6c7bda1229bfa0e05b36bd2f0482215d
shinyelee <shinye0213@naver.com>
2021-01-28 13:16:50 +0900

    GoodsCartListService.java -> MdCartListService.java

--- src/svc/MdCartListService.java
+++ src/svc/MdCartListService.java
@@ -6,12 +6,14 @@ import javax.servlet.http.HttpSession;
 import vo.Cart;
 
 // 장바구니 목록보기 요청을 처리하는 비즈니스 로직을 구현하는 Service 클래스.
-public class GoodsCartListService {
-	
+public class MdCartListService {
+
 	public ArrayList<Cart> getCartList(HttpServletRequest request) {
-		HttpSession session = request.getSession(); // 요청을 한 클라이언트의 세션 객체를 얻어옴.
+		// 요청을 한 클라이언트의 세션 객체를 얻어옴.
+		HttpSession session = request.getSession();
 		// 세션 영역에 공유되어 있는 장바구니 목록 객체를 얻어옴.
 		ArrayList<Cart> cartList = (ArrayList<Cart>)session.getAttribute("cartList");
+		
 		return cartList;
 	}
 	
