5bd7fa5e6c06b1da49bafa63eaaf52d7fb07c380
shinyelee <shinye0213@naver.com>
2021-01-28 22:58:05 +0900

    GoodsCartSearchService.java -> MdCartSearchService.java

--- src/svc/MdCartSearchService.java
+++ src/svc/MdCartSearchService.java
@@ -6,20 +6,21 @@ import javax.servlet.http.HttpSession;
 import vo.Cart;
 
 // 장바구니 항목 검색 요청을 처리하는 비즈니스 로직을 구현하는 Service 클래스.
-public class GoodsCartSearchService {
+public class MdCartSearchService {
 
 	public ArrayList<Cart> getCartSearchList(int start_money, int end_money, HttpServletRequest request) {
-		HttpSession session = request.getSession(); // 요청을 한 클라이언트의 세션 객체를 얻어옴.
+		
+		// 요청을 한 클라이언트의 세션 객체를 얻어옴.
+		HttpSession session = request.getSession();
 		// 세션 영역에 공유되어 있는 장바구니 목록 객체를 얻어옴.
 		ArrayList<Cart> oldCartList = (ArrayList<Cart>)session.getAttribute("cartList");
 		// 검색된 장바구니 항목을 저장할 새로운 ArrayList 객체 생성.
 		ArrayList<Cart> cartList = new ArrayList<Cart>();
-
+		
 		// 장바구니 목록을 반복하면서 검색 범위에 해당하는 장바구니 항목을 찾아서 새로 생성한 ArrayList 객체에 추가.
-		for (int i = 0; i < cartList.size(); i++) {
+		for (int i = 0; i < oldCartList.size(); i++) {
 			// 장바구니 항목 중 가격이 검색 가격에 해당하는지 체크.
-			if(oldCartList.get(i).getPrice()>=start_money &&
-			   oldCartList.get(i).getPrice()<=end_money) {
+			if(oldCartList.get(i).getPrice()>=start_money && oldCartList.get(i).getPrice()<=end_money) {
 				cartList.add(oldCartList.get(i));
 			}
 			
@@ -27,4 +28,5 @@ public class GoodsCartSearchService {
 		
 		return cartList;
 	}
+	
 }
