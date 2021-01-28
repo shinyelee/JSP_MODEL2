99c6e7c943c93d10e19534c091e4c0cdb09e386a
shinyelee <shinye0213@naver.com>
2021-01-28 14:09:46 +0900

    GoodsCartQtyUpService.java -> MdCartQtyUpService.java

--- src/action/MdCartQtyUpAction.java
+++ src/action/MdCartQtyUpAction.java
@@ -11,14 +11,14 @@ public class MdCartQtyUpAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		// 수량을 증가시킬 대상이 되는 장바구니 항목의 item 값을 파라미터 값으로 받음. 장바구니 항목의 식별자는 kind 값을 사용.
+		// 수량을 증가시킬 대상이 되는 장바구니 항목의 item 값을 파라미터 값으로 받음. 장바구니 항목의 식별자는 item 값을 사용.
 		String item = request.getParameter("item");
 		// 장바구니 항목의 수량을 증가시키는 비즈니스 로직을 처리하는 서비스 객체를 생성.
 		MdCartQtyUpService mdCartQtyUpService = new MdCartQtyUpService();
 		// 장바구니 항목의 수량을 증가시키는 메소드를 호출.
 		mdCartQtyUpService.upCartQty(item,request);
 		// 장바구니 항목의 수량을 증가시키는 요청 처리를 실행한 후 포워딩 정보를 ActionForward 객체로 생성.
-		// 장바구니 항목의 수량 증가 처리를 한 후 장바구니 목록보기 요청을 다시 하기 위해 URL은 goodsCartList.go로, 포워딩은 리다이렉트 방식으로 처리하기 위해 true로 지정.
+		// 장바구니 항목의 수량 증가 처리를 한 후 장바구니 목록보기 요청을 다시 하기 위해 URL은 mdCartList.md로, 포워딩은 리다이렉트 방식으로 처리하기 위해 true로 지정.
 		ActionForward forward = new ActionForward("mdCartList.md", true);
 		
 		return forward;
---
+++ src/svc/MdCartQtyUpService.java
@@ -0,0 +1,29 @@
+package svc;
+
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import vo.Cart;
+
+// 장바구니 항목 수량 증가 요청을 처리하는 비즈니스 로직을 구현하는 Service 클래스.
+public class MdCartQtyUpService {
+
+	public void upCartQty(String item, HttpServletRequest request) { 
+		
+		// 요청을 한 클라이언트의 세션 객체를 얻어옴.
+		HttpSession session = request.getSession();
+		// 세션 영역에 공유되어 있는 장바구니 목록 객체를 얻어옴.
+		ArrayList<Cart> cartList = (ArrayList<Cart>)session.getAttribute("cartList");
+		
+		// 수량을 증가시킬 장바구니 항목 객체를 item 값으로 비교해 검색한 후 해당 객체의 수량 값을 증가시킴.
+		for (int i = 0; i < cartList.size(); i++) {
+			
+			if(cartList.get(i).getItem().equals(item)) {
+				cartList.get(i).setQty(cartList.get(i).getQty()+1);		
+			}
+			
+		}
+		
+	}
+	
+}
